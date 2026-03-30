2c94aedb62d581a20c1ffcd98b74c5ee6847c610
Miłosz <101519160+Mpawelo@users.noreply.github.com>
2026-03-30 20:00:00 +0200

    Konfiguracja Program.cs w celu uruchomienia scenariusza

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using APBD1.Repositories;
+using APBD1.Services;
+using APBD1.Menu;
+
+namespace APBD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var equipmentRepo = new EquipmentRepository();
+            var userRepo = new UserRepository();
+            var rentalRepo = new RentalRepository();
+
+            var penaltyCalculator = new PenaltyCalculator();
+            var rentalService = new RentalService(penaltyCalculator);
+
+            var menu = new ConsoleMenu(equipmentRepo, userRepo, rentalRepo, rentalService);
+
+            menu.RunDemonstration();
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
+        }
+    }
+}
